7c854c1f985d69d8e0804320d2e4eb8f786d2353
givemin5 <givemin5@hotmail.com>
2016-10-15 14:20:50 +0800

    10 替 Client 模型類別加上一些必要的商業邏輯驗證

--- MVC5Course/Models/Client.cs
+++ MVC5Course/Models/Client.cs
@@ -11,7 +11,8 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Client
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,11 +20,19 @@ namespace MVC5Course.Models
         {
             this.Order = new HashSet<Order>();
         }
-    
+
         public int ClientId { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage ="不接受 10 個字以上")]
         public string FirstName { get; set; }
+        [Required]
+        [MinLength(5)]
         public string MiddleName { get; set; }
+        [Required]
+        [MaxLength(10)]
         public string LastName { get; set; }
+        [Required]
+        [RegularExpression("[MF]")]
         public string Gender { get; set; }
         public Nullable<System.DateTime> DateOfBirth { get; set; }
         public Nullable<double> CreditRating { get; set; }
